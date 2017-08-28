4797b4551b8bab87f104ef30b38214ae5b263a85
PRO-bits <pguha@student.unimelb.edu.au>
2017-08-28 20:23:03 +1000

    new edit

--- main.c
+++ main.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-int main(int argc, char *argv[])
+struct tree
 {
-	/* code */
-	return 0;
+    char character;
+    int weight;
+    bool flag;
+    struct tree * left;
+    struct tree * equal;
+    struct tree * right;
+};
+
+typedef tree Tree;
+
+int main (int argc, char *argv[]){
+    return 0;
+}
+
+node * insert(node* pNode, char* word, int weight){
+    if (pNode is NULL)
+    {
+        // create a new pNode, and save a character from word
+    }
+
+    if (/*current char in word is smaller than char in pNode*/)
+    {
+        // insert the character on the left branch
+        pNode -> left = insert(pNode -> left, word, weight);
+    }
+
+    else if (/*current char in word is equal to char in pData*/)
+    {
+
+    }
 }
