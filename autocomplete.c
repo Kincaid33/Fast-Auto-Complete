c33d26896f668a802e4c9933bc1388cb6f499e3f
Prodipta Guha <pguha@student.unimelb.edu.au>
2017-08-31 22:53:45 +1000

    new edit

--- autocomplete.c
+++ autocomplete.c
@@ -2,49 +2,34 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-struct tree
+#define WORDLENGTH 250
+
+struct node
 {
     char character;
     int weight;
-    bool flag;
+    bool end_of_key;
     struct tree * left;
     struct tree * equal;
     struct tree * right;
 };
 
-typedef tree Tree;
+typedef node node;
 
 /*function prototypes*/
 node * insert(node* pNode, char* word, int weight);
-
+/******************************/
 int main (int argc, char *argv[]){
-
-    Tree.flag = false;
-
-    
-
-
-
+    char *temp = malloc((WORDLENGTH+1)*sizeof(char));
+    char *temp = "Beautiful";
+    node pData;
+    pData->end_of_key = false;
+    root = insert(root, "cat", 3);
     return 0;
 }
 
 node * insert(node* pNode, char* word, int weight){
-    if (pNode is NULL)
-    {
-        // create a new pNode, and save a character from word
-    }
-
-    if (/*current char in word is smaller than char in pNode*/)
-    {
-        // insert the character on the left branch
-        pNode -> left = insert(pNode -> left, word, weight);
-    }
-
-    else if (/*current char in word is equal to char in pNode*/)
-    {
-        if (/*next char in word is '\0'*/)
-        {
-            // set pNode end_of_key_flag to true and assign weight
-        }
+    if (pNode == NULL){
+        pNode.Data->word[];
     }
 }
